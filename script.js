10acda939a75d805bbd90406957c3b49dd358b99
OshanFPS <poudeloshan951@gmail.com>
2025-05-13 07:06:30 +0545

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const toggleButton = document.querySelector('.theme-toggle button');
 toggleButton?.addEventListener('click', () => {
   document.body.classList.toggle('light-mode');
 });
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
